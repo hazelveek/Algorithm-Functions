e1d59cd21a69f92268fc7e2b24f5a1d5b838a486
Victoria Etim <viktoriaetim@gmail.com>
2019-11-05 12:21:32 +0100

    Solved the reverse-integer algorithm

---
+++ reverse-integer-without-casting-to-string.js
@@ -0,0 +1,20 @@
+/**
+ * Given a positive number, reverse the number without first casting to string
+ * @param {number} n  
+ */
+function reverseInt(n){
+    let result = n, remainder, newNumber, divisor = 10;
+    while(result > 0){
+      remainder = result%divisor;
+      if(!newNumber){
+        newNumber = remainder * 1;
+      }
+      else{
+        newNumber = (newNumber * 10) + remainder
+      }
+      result = Math.floor(result/divisor)
+      console.log({newNumber, remainder, result, divisor});
+    }
+    return newNumber;
+}
+  
